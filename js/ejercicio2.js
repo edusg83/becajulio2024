0db7346d16864946bdb0b30ee5e783d6221c12c2
lauselfe <lauselfe@gmail.com>
2024-09-16 17:06:01 +0200

    ejercicio 3

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -3,3 +3,11 @@ let ancho = 12.203004;
 let altoPorAncho = alto * ancho; 
 console.log(altoPorAncho); 
 console.log(altoPorAncho.toFixed(2)); 
+
+//Ejercicio 3
+
+let cadena1 = "CADENA1"; 
+let ena = cadena1.indexOf("ena"); 
+let last = cadena1.charAt(last.length - 1); 
+let minusculas = cadena1.toLocaleLowerCase(); 
+let camena1 = cadena1.replace("de", "me"); 
