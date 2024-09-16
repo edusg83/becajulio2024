7bac4f52a9c98829247362904809d5847b76146b
lauselfe <lauselfe@gmail.com>
2024-09-16 17:07:23 +0200

    ejercicio 3

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -11,3 +11,5 @@ let ena = cadena1.indexOf("ena");
 let last = cadena1.charAt(last.length - 1); 
 let minusculas = cadena1.toLocaleLowerCase(); 
 let camena1 = cadena1.replace("de", "me"); 
+let cadena2 = "Cadena2"; 
+let lasDos = cadena1 + cadena2; 
