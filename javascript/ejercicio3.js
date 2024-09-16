b81fab8efd863cf382aafe749a12955684bd69f1
Carlos <email>
2024-09-16 16:52:59 +0200

    ej3

---
+++ javascript/ejercicio3.js
@@ -0,0 +1,10 @@
+let cadena1 = "CADENA1";
+console.log(cadena1);
+
+console.log(cadena1.indexOf("ENA"));
+console.log(cadena1.charAt(6));
+console.log(cadena1.toLowerCase());
+console.log(cadena1.replace("DE","ME"));
+
+let cadena2 = "CADENA2";
+console.log(cadena1+cadena2);
