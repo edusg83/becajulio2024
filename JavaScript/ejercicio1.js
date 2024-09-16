24b1469851dca19a044d10f5faf27e3b37010714
EstherRuizSempere <estruisem@alu.edu.gva.es>
2024-09-16 17:08:11 +0200

    cadena

--- JavaScript/ejercicio1.js
+++ JavaScript/ejercicio1.js
@@ -5,10 +5,19 @@ console.log("Hola log");
 console.time();
 */
 
+/*
 let alto = new Number(12);
 let ancho = new Number(12.203004);
 let altoPorAncho = alto * ancho;
 
 console.log(altoPorAncho);
 console.log(altoPorAncho.toFixed(2));
+*/
 
+let cadena1 ="CADENA1";
+let cadena2= "CADENA2";
+console.log(cadena1.charAt(3, 5));
+console.log(cadena1.lastIndexOf);
+console.log(cadena1.toLowerCase);
+console.log(cadena1.replace("DE", "ME"));
+console.log(cadena1 +(" ")+ cadena2);
