0779f2b35a4ce3c6b48cb8c42fa05fdf94889b74
rubenbanegas <sargolandia@gmail.com>
2024-09-16 18:17:28 +0200

    ejercicio4

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,6 +1,6 @@
 let cadena1 = "CADENA1";
 console.log(cadena1.indexOf("ENA"));
-console.log(cadena1.substring(6));
+console.log(cadena1.charAt(cadena1.length - 1));
 console.log(cadena1.toLowerCase());
 console.log(cadena1.replace("DE", "ME"));
 let cadena2 = "CADENA2, " + cadena1;
---
+++ js/ejercicio4.js
@@ -0,0 +1,20 @@
+let cadena1 = "CADENA1";
+let cadena2;
+console.log(cadena1);
+console.log(cadena2);
+
+if (cadena2 == undefined) {
+    console.log("SIN DEFINIR");
+}
+
+let cliente = {
+    nombre: "PEPITO",
+    tel: "656666666",
+    direccion: "C/Salud,21",
+}
+
+console.log(cliente.nombre + ", " + cliente.tel + ", " + cliente.direccion);
+
+for(propiedad in cliente){
+    console.log(cliente[propiedad]);
+}
