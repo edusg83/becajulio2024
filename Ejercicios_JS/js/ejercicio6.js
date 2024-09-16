14c08f9413c52da3360bd272bdcfeb91a9568fab
Nicolas <nicogilruz98@gmail.com>
2024-09-16 18:55:50 +0200

    __̴ı̴̴̡̡̡ ̡͌l̡̡̡ ̡͌l̡*̡̡ ̴̡ı̴̴̡ ̡̡͡|̲̲̲͡͡͡ ̲▫̲͡ ̲̲̲͡͡π̲̲͡͡ ̲̲͡▫̲̲͡͡ ̲|̡̡̡ ̡ ̴̡ı̴̡̡ ̡͌l̡̡̡̡.___

---
+++ Ejercicios_JS/js/ejercicio6.js
@@ -0,0 +1,30 @@
+// a) Crear una variable de tipo array llamada array1 con valor [1,2,3,4]
+let array1 = [1, 2, 3, 4];
+
+// b) Crear un pequeño programa que recorra el array y cuando encuentre el 1 muestre UNO , el 2 muestre DOS, el 3 muestre TRES, el 4 muestre CUATRO Utilizar la instrucción Switch
+for (let i = 0; i < array1.length; i++) {
+    switch (num) {
+        case 1:
+            console.log("UNO");
+            break;
+        case 2:
+            console.log("DOS");
+            break;
+        case 3:
+            console.log("TRES");
+            break;
+        case 4:
+            console.log("CUATRO");
+            break;
+    }
+}
+
+// c) Introducir un 3 al final del array y visualizar el array
+array1.push(3);
+console.log(array1);
+
+// d) Quitar los 3 últimos elementos del array y visualizar el array
+for (let i = 0; i < 3; i++) {
+    array1.pop();
+}
+console.log(array1);
