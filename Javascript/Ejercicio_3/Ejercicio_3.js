51d82c3cf5b3718677371a5ecc25ae9058c02212
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-16 17:04:15 +0200

    chicken commit

---
+++ Javascript/Ejercicio_3/Ejercicio_3.js
@@ -0,0 +1,10 @@
+let cadena1 = "CADENA1"
+
+console.log(cadena1)
+console.log(cadena1.substring(3,6))
+console.log(cadena1.charAt(cadena1.length))
+console.log(cadena1.toLowerCase)
+
+cadena1.replace("DE","ME")
+
+console.log(cadena1)
