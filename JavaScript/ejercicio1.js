48e480fde2cab6618e4aa4fec81ccb0446eb2959
EstherRuizSempere <estruisem@alu.edu.gva.es>
2024-09-16 18:39:29 +0200

    array

--- JavaScript/ejercicio1.js
+++ JavaScript/ejercicio1.js
@@ -26,7 +26,7 @@ console.log(cadena1.replace("DE", "ME"));
 console.log(cadena1 +(" ")+ cadena2);
 */
 
-let cadena1 = "CADENA1";
+/*let cadena1 = "CADENA1";
 let cadena2;
 console.log(typeof cadena1);
 console.log(typeof cadena2);
@@ -42,4 +42,30 @@ let cliente1 = {
 
 cliente1.direccion = "Desengaño 21";
 
-console.log(cliente1.nombre, cliente1.tel, cliente1.direccion);
+console.log(cliente1.nombre, cliente1.tel, cliente1.direccion);*/
+
+for (let i = 0; i < array1.length; i++) {
+    let numero = array1[i];
+
+    switch (numero) {
+        case 1:
+            console.log("UNO");
+            break;
+        case 2:
+            console.log("DOS");
+            break;
+        case 3:
+            console.log("TRES");
+            break;
+        case 4:
+            console.log("CUATRO");
+            break;
+        default:
+            console.log("No se sabe el número");
+    }
+}
+array1 = array1.concat(3);
+console.log(array1);
+
+array1.length -= 3;
+console.log(array1);
