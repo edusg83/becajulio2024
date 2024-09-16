bc343a4c62ca5fd6ef0c8b3959f45455071fd646
Carlos <email>
2024-09-16 17:09:24 +0200

    ej4

---
+++ javascript/ejercicio4.js
@@ -0,0 +1,19 @@
+let cadena1 = "CADENA1",
+cadena2;
+
+console.log(typeof(cadena1));
+console.log(typeof(cadena2));
+
+if(typeof(cadena2) == "undefined"){
+    cadena2 = "SIN DEFINIR";
+}
+
+console.log(cadena2);
+
+let cliente = { nombre : "PEPITO", tel: "656666666"};
+console.log(cliente);
+
+cliente.direccion = "C/ Salud, 21";
+console.log(cliente);
+
+
