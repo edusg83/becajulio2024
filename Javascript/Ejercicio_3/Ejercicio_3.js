ccbae703455f3d0daed01e9a9ccb33cf3abbe2aa
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-16 17:06:40 +0200

    commi

--- Javascript/Ejercicio_3/Ejercicio_3.js
+++ Javascript/Ejercicio_3/Ejercicio_3.js
@@ -2,9 +2,14 @@ let cadena1 = "CADENA1"
 
 console.log(cadena1)
 console.log(cadena1.substring(3,6))
-console.log(cadena1.charAt(cadena1.length))
-console.log(cadena1.toLowerCase)
+console.log(cadena1.charAt(cadena1.length - 1))
+
+console.log(cadena1.toLowerCase())
 
 cadena1.replace("DE","ME")
 
 console.log(cadena1)
+
+let cadena2 = "CADENA2"
+
+console.log(cadena1 + cadena2)
