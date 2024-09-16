af73e1e4fe60b7e1deb0a177dfaf41f2cd58cd6f
Jesús Terrés <jesusterres1999@gmail.com>
2024-09-16 16:26:41 +0200

    Ejercicio 1 JS

---
+++ Ejercicios JavaScript/Ejercicio 1/js/ejercicio1.js
@@ -0,0 +1,12 @@
+console.info("MENSAJE DE INFO");
+
+console.warn("MENSAJE DE WARNING");
+
+console.error("MENSAJE DE ERROR");
+
+console.log("MENSAJE DE LOG");
+
+console.time("TIEMPO");
+setTimeout(() => {
+    console.timeEnd("TIEMPO");
+}, 1000); 
