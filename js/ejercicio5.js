d97d3738ab584bf80d82e859705a215927bbaa88
rubenbanegas <sargolandia@gmail.com>
2024-09-16 18:55:18 +0200

    ejercicio6

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,4 +1,4 @@
-let array1 = [1,2,3,4,3];
+let array1 = [1,2,3,4];
 
 for(let i = 0; i <= array1.length; i++) {
     switch(array1[i]){
@@ -16,3 +16,8 @@ for(let i = 0; i <= array1.length; i++) {
             break;
     }
 }
+
+array1.push(3);
+
+array1.length = array1.length - 3;
+console.log(array1);
---
+++ js/ejercicio6.js
@@ -0,0 +1,36 @@
+let array1 = [1,2,3,4];
+
+for(let num of array1) {
+    switch(num){
+        case 1:
+            console.log("UNO");
+            break;
+        case 2:
+            console.log("DOS");
+            break;
+        case 3:
+            console.log("TRES");
+            break;
+        case 4:
+            console.log("CUATRO");
+            break;
+    }
+}
+
+
+for(let i in array1) {
+    switch(array1[i]){
+        case 1:
+            console.log("UNO");
+            break;
+        case 2:
+            console.log("DOS");
+            break;
+        case 3:
+            console.log("TRES");
+            break;
+        case 4:
+            console.log("CUATRO");
+            break;
+    }
+}
