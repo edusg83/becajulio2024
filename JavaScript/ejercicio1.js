cdce61c685795d7c0429312ded2424859ee153e3
EstherRuizSempere <estruisem@alu.edu.gva.es>
2024-09-16 17:08:47 +0200

    a

--- JavaScript/ejercicio1.js
+++ JavaScript/ejercicio1.js
@@ -16,8 +16,8 @@ console.log(altoPorAncho.toFixed(2));
 
 let cadena1 ="CADENA1";
 let cadena2= "CADENA2";
-console.log(cadena1.charAt(3, 5));
-console.log(cadena1.lastIndexOf);
+console.log(cadena1.indexOf("ENA"));
+console.log(cadena1.lastIndexOf());
 console.log(cadena1.toLowerCase);
 console.log(cadena1.replace("DE", "ME"));
 console.log(cadena1 +(" ")+ cadena2);
