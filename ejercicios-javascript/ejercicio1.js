f23c7c659571fad0f9859963ba043ed180f199d0
fabrizio <fabrizio3522@gmail.com>
2024-09-16 15:54:43 +0100

    014

---
+++ ejercicios-javascript/ejercicio1.js
@@ -0,0 +1,7 @@
+console.time();
+console.info("Info");
+console.warn("Warning");
+console.error("error");
+console.log("Hello World");
+console.timeEnd();
+
---
+++ ejercicios-javascript/ejercicio2.js
@@ -0,0 +1,7 @@
+var alto = new Number(12),
+    ancho = new Number(12.203004),
+    altoPorAncho = new Number(ancho*alto);
+
+console.log(altoPorAncho);
+
+console.log(altoPorAncho.toFixed(2));
