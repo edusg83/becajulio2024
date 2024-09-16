ae1b69a56c92857be52cfe3fd10bcc1d25da7a3b
Cindy Zacarias <cindyjzacarias@gmail.com>
2024-09-16 17:17:38 +0200

    js

---
+++ JS_EJE/Ejercicio_3/ejercicio3.js
@@ -0,0 +1,10 @@
+let cadena1 = "CADENA1";
+console.log(cadena1);
+console.log(cadena1.substring(3,6));
+console.log(cadena1.charAt(cadena1.length - 1));
+console.log(cadena1.toLowerCase());
+cadena1 = cadena1.replace("DE","ME");
+console.log(cadena1);
+cadena1 = "CADENA1";
+let cadena2 = "CADENA2";
+console.log(cadena1 + cadena2);
