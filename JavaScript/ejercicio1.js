a2d4da0f8dad8c843d2df64c088f9b5a3609538c
EstherRuizSempere <estruisem@alu.edu.gva.es>
2024-09-16 16:41:53 +0200

    ejercicio2

--- JavaScript/ejercicio1.js
+++ JavaScript/ejercicio1.js
@@ -1,5 +1,14 @@
-console.info("Hola info");
+/*console.info("Hola info");
 console.warn("Hola warning");
 console.error("Hola error");
 console.log("Hola log");
-console.time("Hola tiempo");
+console.time();
+*/
+
+var alto = new Number(12);
+var ancho = new Number(12.203004);
+var altoPorAncho = alto * ancho;
+
+console.log(altoPorAncho);
+console.log(altoPorAncho.toFixed);
+
