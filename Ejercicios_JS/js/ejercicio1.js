17e99309a62d36e2dce3d5c5d0d3e62e2f85d803
Nicolas <nicogilruz98@gmail.com>
2024-09-16 16:42:04 +0200

    Commit

--- Ejercicios_JS/js/ejercicio1.js
+++ Ejercicios_JS/js/ejercicio1.js
@@ -4,3 +4,10 @@ console.error("MENSAJE DE ERROR");
 console.log("MENSAJE DE LOG");
 console.time();
 console.timeEnd();
+
+let numero = 69,
+    mensaje = "Hola Sema!",
+    enable = true,
+    hoy = new Date();
+
+    
---
+++ Ejercicios_JS/js/ejercicio2.js
@@ -0,0 +1,12 @@
+// a) Declarar una variable numérica llamada alto con valor 12 y otra variable numérica llamada ancho con valor 12.203004
+let alto = Number(12);
+let ancho = Number(12.203004);
+
+// b) Declarar una variable numérica llamada altoPorAncho con valor resultado de multiplicar la variable alto por la variable ancho
+let altoPorAncho = Number(alto * ancho);
+
+// c) Mostrar por consola el valor de la variable altoPorAncho
+console.log(altoPorAncho);
+
+// d) Mostrar por consola el valor de la variable altoPorAncho redondeando a 2 decimales
+console.log(altoPorAncho.toFixed(2));
