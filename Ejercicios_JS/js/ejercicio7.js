b57028c7621fea377db2efd9b34ed4b676b1a443
Nicolas <nicogilruz98@gmail.com>
2024-09-16 19:45:52 +0200

    ¦̵̱ ̵̱ ̵̱ ̵̱ ̵̱(̢ ̡͇̅└͇̅┘͇̅ (▤8כ−◦

--- Ejercicios_JS/js/ejercicio7.js
+++ Ejercicios_JS/js/ejercicio7.js
@@ -15,5 +15,5 @@ console.log(pieza1.peso === pieza2.peso);
 
 
 // d) Con el operador ternario , sumar el valor de las propiedades peso de los objetos pieza1 y pieza2 si la propiedad peso de pieza1='20', multiplizar en caso contrario
-let resultado = (pieza1.peso === 20) ? (pieza1.peso + pieza2.peso) : (pieza1.peso * pieza2.peso);
+let resultado = (pieza1.peso === pieza2.peso) ? (pieza1.peso + Number(pieza2.peso)) : (pieza1.peso * Number(pieza2.peso));
 console.log(resultado);
