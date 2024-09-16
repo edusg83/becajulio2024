ed6b422149945698ed27c476303c21e86202093e
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-16 19:35:39 +0200

    break

---
+++ Javascript/Ejercicio_7/Ejercicio_7.js
@@ -0,0 +1,15 @@
+let pieza = {
+    peso:20,
+}   
+
+let pieza2 = {
+    peso:'20'
+}
+
+console.log(pieza.peso == pieza2.peso)
+
+console.log(pieza.peso === pieza2.peso)
+
+let x = (pieza.peso == '20') ? pieza.peso + pieza2.peso : pieza.peso * pieza2.peso;
+
+console.log(x)
