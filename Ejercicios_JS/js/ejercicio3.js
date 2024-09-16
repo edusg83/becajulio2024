5c9873ee67bcb9eb794d38c37655b6f3f1240344
Nicolas <nicogilruz98@gmail.com>
2024-09-16 17:04:49 +0200

    Commit

---
+++ Ejercicios_JS/js/ejercicio3.js
@@ -0,0 +1,18 @@
+// a) Crear una variabe de tipo cadena llamada cadena1 con valor CADENA1
+let cadena1 = "CADENA1";
+
+// b) La posición del texto "ENA" en la variable cadena1
+console.log(cadena1.indexOf("ENA"));
+
+// c) El carácter de la última posición de la cadena
+console.log(cadena1.charAt(cadena1.length - 1));
+
+// d) El valor de la variable cadena1 convertido a minúsculas
+console.log(cadena1.toLowerCase());
+
+// e) Sustituir en cadena1 "DE" por "ME“
+console.log(cadena1.replace(/["DE"]/gi, "ME"));
+
+// f) Crear una variable de tipo cadena llamada cadena2 con valor CADENA2 y concatenar cadena2 con cadena1
+let cadena2 = "CADENA2";
+console.log(cadena1 + cadena2);
