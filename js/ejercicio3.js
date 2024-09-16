aaee38fe11a439193ca80202e2e524adc8256541
rubenbanegas <sargolandia@gmail.com>
2024-09-16 17:07:56 +0200

    ejercicio3

---
+++ js/ejercicio3.js
@@ -0,0 +1,7 @@
+let cadena1 = "CADENA1";
+console.log(cadena1.indexOf("ENA"));
+console.log(cadena1.substring(6));
+console.log(cadena1.toLowerCase());
+console.log(cadena1.replace("DE", "ME"));
+let cadena2 = "CADENA2, " + cadena1;
+console.log(cadena2);
