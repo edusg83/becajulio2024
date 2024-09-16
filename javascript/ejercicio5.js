3896cf123efeae0063892d0be4362c82f85b6eb3
Carlos <email>
2024-09-16 17:43:08 +0200

    commit ej 5

--- javascript/ejercicio5.js
+++ javascript/ejercicio5.js
@@ -1,24 +1,64 @@
 let array1 = [1,2,3,4];
 console.log(array1);
 
-for (let i = 0; i < array1.length; i++) {
-    switch (array1[i]) {
-        case 1:
-            console.log("UNO");
-            break;
-        case 2:
-            console.log("DOS");
-            break;
-        case 3:
-            console.log("TRES");
-            break;
-        case 4:
-            console.log("CUATRO");
-            break;
-        default:
-            console.log("Número no reconocido");
-            break;
-    }
+switch (array1[0]) {
+    case 1:
+        console.log("UNO");
+        break;
+    case 2:
+        console.log("DOS");
+        break;
+    case 3:
+        console.log("TRES");
+        break;
+    case 4:
+        console.log("CUATRO");
+        break;
+}
+
+switch (array1[1]) {
+    case 1:
+        console.log("UNO");
+        break;
+    case 2:
+        console.log("DOS");
+        break;
+    case 3:
+        console.log("TRES");
+        break;
+    case 4:
+        console.log("CUATRO");
+        break;
+}
+
+switch (array1[2]) {
+    case 1:
+        console.log("UNO");
+        break;
+    case 2:
+        console.log("DOS");
+        break;
+    case 3:
+        console.log("TRES");
+        break;
+    case 4:
+        console.log("CUATRO");
+        break;
+}
+
+switch (array1[3]) {
+    case 1:
+        console.log("UNO");
+        break;
+    case 2:
+        console.log("DOS");
+        break;
+    case 3:
+        console.log("TRES");
+        break;
+    case 4:
+        console.log("CUATRO");
+        break;
 }
 
 array1[4] = 3;
