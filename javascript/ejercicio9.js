56b3a906674d9c626595964aad3ee8b5d585b04d
Carlos <email>
2024-09-17 15:21:12 +0200

    ej9

---
+++ javascript/ejercicio9.js
@@ -0,0 +1,26 @@
+var color = "Azul";
+function experimento() {
+    console.log(color);
+}
+experimento();
+var color = "Verde";
+experimento();
+
+var color2 = "Azul";
+function experimento() {
+    var color2 = "Amarillo";
+    console.log(color2);
+}
+experimento();
+var color2 = "Verde";
+experimento();
+
+var color3 = "Azul";
+function experimento() {
+    var color3 = "Amarillo";
+    console.log(color3);
+}
+experimento();
+var color3 = "Verde";
+experimento();
+console.log(color3);
