a0e450159d6910e3fd9ca008cf2c4fe26f7442b0
lauselfe <lauselfe@gmail.com>
2024-09-17 18:00:38 +0200

    calculadora sin eval u.u

--- js/calculadora.js
+++ js/calculadora.js
@@ -25,9 +25,85 @@ function clearLastChar(){
   display.value =  display.value.trim().slice(0,-1); 
 }
 
+function getNumbers(expression){
+    let numbers = []; 
+    let currentNumber = '';
+    for(let i =0; i < expression.length; i++){
+        let char = expression[i]; 
+        if(!isNaN(char) || char === '.'){
+            currentNumber += char; 
+        }else if(['+', '-', '*', '/'].includes(char)){
+            if(currentNumber){
+                numbers.push(parseFloat(currentNumber)); 
+                currentNumber = ''; 
+            }
+        }
+    }
+
+    if (currentNumber) {
+        numbers.push(parseFloat(currentNumber));
+    }
+
+    return numbers; 
+}
+
+function getOperators(expression){
+    let operators = [];
+
+    for (let i = 0; i < expression.length; i++) {
+        let char = expression[i];
+        if (['+', '-', '*', '/'].includes(char)) {
+            operators.push(char);
+        }
+    }
+
+    return operators; 
+}
+
+function calculateMultiplicationOrDivision(operators, numbers){
+    let i = 0;
+    while(i < operators.length){
+        if(operators[i] === '*' || operators[i] === '/'){
+            let result = operators[i] === '*'
+                ? numbers[i] * numbers[i + 1]
+                : numbers[i] / numbers[i + 1]; 
+    
+                numbers.splice(i, 2, result); 
+                operators.splice(i, 1); 
+        } else{
+            i++; 
+        }
+    } 
+  
+}
+
+function calculateAdditionOrSubstraction(operators, numbers){
+    let i = 0;
+    while(i < operators.length){
+
+        let result = operators[i] === '+'
+                    ? numbers[i] + numbers[i + 1]
+                    : numbers[i] - numbers[i + 1]; 
+        numbers.splice(i, 2, result); 
+        operators.splice(i, 1); 
+    
+    }
+    
+}
+
 function calculate(){
     try{
-        display.value = eval(display.value); 
+      //  display.value = eval(display.value); 
+        let expression = display.value;
+        let numbers = getNumbers(expression);
+        let operators = getOperators(expression);
+    
+        calculateMultiplicationOrDivision(operators, numbers);  
+        calculateAdditionOrSubstraction(operators, numbers); 
+        
+        //Muestra el resultado
+        display.value = numbers[0]; 
+      
     }catch(error){
         display.value = 'Error'; 
     }
