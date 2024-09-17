b5b75a2b213818a3758e9130513ee03ccf1113ae
Juarety <joaquingomezmar15@icloud.com>
2024-09-17 15:47:35 +0200

    ejercicio10

---
+++ js/ejercicio10.js
@@ -0,0 +1,13 @@
+var funciones = {
+    colorVerde: function(){
+        console.log("soy verde")
+    },
+    colorRojo: function(){
+        console.log("soy rojo")
+    }
+
+}
+
+funciones.colorRojo;
+
+funciones.colorVerde;
