e276154d0134165253d0b1cd75dff8d9976dbca9
EstherRuizSempere <estruisem@alu.edu.gva.es>
2024-09-17 15:46:50 +0200

    colores

--- JavaScript/ejercicio8.js
+++ JavaScript/ejercicio8.js
@@ -1,4 +1,4 @@
-function suma (param1, param2){
+/*function suma (param1, param2){
     return param1 + param2;
 }
 
@@ -6,4 +6,18 @@ console.log(suma(12, 12));
 
 (function(param1, param2){
     console.log(param1+param2);
-})(12,12);
+})(12,12);*/
+
+let funciones = {
+   colorVerde : function(){
+    console.log("verde");
+   },
+
+   colorRojo : function(){
+    console.log("rojo");
+   }
+};
+
+funciones.colorVerde();
+
+funciones.colorRojo();
