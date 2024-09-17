c9bf94f033edb6baa6bf37bfbc758b5ca32f8a59
Nicolas <nicogilruz98@gmail.com>
2024-09-17 19:58:12 +0200

    Commit

--- Ejercicios_JS/js/calculadora.js
+++ Ejercicios_JS/js/calculadora.js
@@ -2,6 +2,7 @@ let num1 = "";
 let num2 = "";
 let operador;
 let resultado = 0;
+let memoria = 0;
 
 function colocarNumero(num) {
     if (operador == undefined) {
@@ -55,4 +56,31 @@ function calcularResultado() {
     num1 = "";
     num2 = "";
     operador = undefined;
+}
+
+function sumarMemoria() {
+    if (operador == undefined) {
+        memoria = document.getElementById("resultado").innerHTML;
+    }
+}
+
+function mostrarMemoria() {
+    if (operador == undefined) {
+        num1 = memoria;
+        document.getElementById("resultado").innerHTML = num1;
+    } else {
+        num2 = memoria;
+        document.getElementById("resultado").innerHTML = num1 + " " + operador + " " + num2;
+    }
+}
+
+function borrarMemoria() {
+    memoria = 0;
+}
+
+function reset() {
+    num1 = "";
+    num2 = "";
+    operador = undefined;
+    document.getElementById("resultado").innerHTML = 0;
 }
