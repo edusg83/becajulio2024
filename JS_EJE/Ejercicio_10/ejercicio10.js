8fd6196a05b8cccba67dae089bfa7d9066eacdad
Cindy Zacarias <cindyjzacarias@gmail.com>
2024-09-17 15:46:47 +0200

    10

---
+++ JS_EJE/Ejercicio_10/ejercicio10.js
@@ -0,0 +1,12 @@
+
+let funciones = {
+    colorVerde: function(){
+        console.log("VERDE")
+    },
+    colorRojo: function(){
+        console.log("ROJO")
+    }
+}
+
+funciones.colorRojo();
+funciones.colorVerde();
