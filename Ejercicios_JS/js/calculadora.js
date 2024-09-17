c7a0676a50662d034b3d68c6d506a3ddd449d3d3
Nicolas <nicogilruz98@gmail.com>
2024-09-17 17:24:36 +0200

    °j°m

--- Ejercicios_JS/js/calculadora.js
+++ Ejercicios_JS/js/calculadora.js
@@ -1,42 +1,52 @@
-let num1 = 0;
-let num2 = 0;
+let num1 = "";
+let num2 = "";
 let operador;
 let resultado = 0;
 
 function colocarNumero(num) {
     if (operador == undefined) {
-        num1 = num;
+        num1 += num;
         document.getElementById("resultado").innerHTML = num1;
     } else {
-        num2 = num;
+        num2 += num;
         document.getElementById("resultado").innerHTML = num1 + " " + operador + " " + num2;
     }
 }
 
 function colocarOperador(operator) {
-    if (num1 != 0) {
+    if (num1 != "") {
         operador = operator;
         document.getElementById("resultado").innerHTML = num1 + " " + operador;
     }
 }
 
-function colocarComa(){
-    
+function colocarComa() {
+    if (operador == undefined) {
+        if (!num1.includes(".")) {
+            num1 += ".";
+            document.getElementById("resultado").innerHTML = num1;
+        }
+    } else {
+        if (!num2.includes(".")) {
+            num2 += ".";
+            document.getElementById("resultado").innerHTML = num1 + " " + operador + " " + num2;
+        }
+    }
 }
 
 function calcularResultado() {
     switch (operador) {
         case "+":
-            resultado = num1 + num2;
+            resultado = parseFloat(num1) + parseFloat(num2);
             break;
         case "-":
-            resultado = num1 - num2;
+            resultado = parseFloat(num1) - parseFloat(num2);
             break;
         case "X":
-            resultado = num1 * num2;
+            resultado = parseFloat(num1) * parseFloat(num2);
             break;
         case "/":
-            resultado = num1 / num2;
+            resultado = parseFloat(num1) / parseFloat(num2);
             break;
     }
 
