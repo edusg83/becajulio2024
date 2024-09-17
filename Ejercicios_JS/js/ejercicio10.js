80669a9cb21fecd64c6f4db5d7f4bf88ed64df09
Nicolas <nicogilruz98@gmail.com>
2024-09-17 15:45:03 +0200

    Commit

---
+++ Ejercicios_JS/js/ejercicio10.js
@@ -0,0 +1,23 @@
+/* a) Crear un objeto llamado funciones e incluir una propiedad que sea una función llamada colorVerde que cuando se invoque
+ muestre por consola “VERDE”. Invocar la función colorVerde */
+let funciones = {
+    colorVerde: function(){
+        console.log("VERDE");
+    }
+};
+
+funciones.colorVerde();
+
+// b) Añadir al Objeto funciones otra propiedad función llamada colorRojo que cuando se invoque muestre por consola “ROJO”
+
+funciones = {
+    colorVerde: function(){
+        console.log("VERDE");
+    },
+    colorRojo: function(){
+        console.log("ROJO");
+    }
+};
+
+funciones.colorVerde();
+funciones.colorRojo();
---
+++ Ejercicios_JS/js/ejercicio9.js
@@ -0,0 +1,32 @@
+var color = "Azul";
+
+function experimento() {
+    console.log(color);
+}
+
+experimento();
+var color = "Verde";
+experimento();
+
+//-------------------------------------------------------------------------------------------
+
+// var color = "Azul";
+// function experimento() {
+//     var color = "Amarillo";
+//     console.log(color);
+// }
+// experimento();
+// var color = "Verde";
+// experimento();
+
+//-------------------------------------------------------------------------------------------
+
+// var color = "Azul";
+// function experimento() {
+//     var color = "Amarillo";
+//     console.log(color);
+// }
+// experimento();
+// var color = "Verde";
+// experimento();
+// console.log(color);
