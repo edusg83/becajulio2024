688802413909c0990d5ad38498289964ce4e1c92
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-18 23:30:13 +0200

    HUH?

--- Parejas/scripts.js
+++ Parejas/scripts.js
@@ -1,5 +1,24 @@
+let ArrayCards = Array.from(document.getElementsByClassName("card"));
+
+// Hacer un mapa donde guarde key y url
+
+// Asignar la key de forma aleatoria con cada partida
+
 function randomGenerator() {
-  return Math.floor(Math.random() * 17);
+  return Math.floor(Math.random() * 8 + 1);
 }
 
-console.log(randomGenerator());
+function asignValue() {
+  ArrayCards.forEach((element) => {
+    element.value = randomGenerator();
+  });
+}
+
+function toggleEffect(param) {
+  console.log(param);
+  let button = document.getElementById(param);
+  console.log(button);
+  button.classList.toggle("test");
+  console.log(button);
+}
+console.log(ArrayCards);
