3633555327ab453b0cda65ad95b9372ac428a8df
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-18 15:26:10 +0200

    I got a commit for you

--- Calculadora/scripts.js
+++ Calculadora/scripts.js
@@ -3,6 +3,7 @@ let num2 = 0;
 let memory = 0;
 let symbol = "";
 let firstOperation = true;
+let symbolPressed = false;
 let display;
 
 function setDisplay(param) {
@@ -27,15 +28,17 @@ function storeValue() {
 function storeSymbol(param) {
   if (symbol == "") {
     symbol = param;
+    symbolPressed = true;
   }
 }
 
 function deleteOperation() {
-  num1 = undefined;
-  num2 = undefined;
-  symbol = undefined;
+  num1 = 0;
+  num2 = 0;
+  symbol = "";
   display.value = "0";
   firstOperation = true;
+  symbolPressed = false;
 }
 
 function displayMemory() {
@@ -47,10 +50,10 @@ function displayMemory() {
 
 function addMemory() {
   display = document.getElementById("display");
-  if (display.value != "0") {
+  if (display.value != "0" && !symbolPressed) {
     memory += Number(display.value);
+    display.value = 0;
   }
-  display.value = 0;
 }
 
 function clearMemory() {
---
+++ Javascript/Ejercicio_13/Ejercicio_13.js
@@ -0,0 +1,5 @@
+try {
+  aleeeert("Holamundo!");
+} catch (err) {
+  console.warn("ERROR " + err);
+}
