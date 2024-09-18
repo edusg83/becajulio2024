00e84061206ee19253015387bc838767e0999705
Cindy Zacarias <cindyjzacarias@gmail.com>
2024-09-18 15:57:15 +0200

    let

--- JS_EJE/Ejercicio_14/ejercicio14.js
+++ JS_EJE/Ejercicio_14/ejercicio14.js
@@ -9,14 +9,14 @@ let objetoPadre = {
 
 /* Creamos hijo1 a partir de objetoPadre */
 
-var hijo1=Object.create(objetoPadre);
+let hijo1=Object.create(objetoPadre);
 hijo1.edad=21;
 hijo1.nombre="Antonio";
 hijo1.apellidos="Martinez Garcia";
 
 /* Creamos hijo2 a partir de objetoPadre */
 
-var hijo2=Object.create(objetoPadre);
+let hijo2=Object.create(objetoPadre);
 hijo2.edad=30;
 hijo2.nombre="Pepe";
 hijo2.apellidos="Martinez Garcia";
@@ -26,7 +26,7 @@ hijo2.muestraDatos();
 /*hijo1.verEdad(); hijo2.verEdad();*/
 /* Creamos una función nueva del Objeto Padre que será heredada por hijo1 e hijo2 a través del prototype */
 
-objetoPadre["verEdad"]=function(){
+objetoPadre["verEdad"]=function() {
 console.log(this.edad);
 }
 hijo1.verEdad();
