d842902ddf26423ca16befb64edf6eb99fe3eaf8
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-18 14:56:13 +0200

    nuevos botones

--- Calculadora/scripts.js
+++ Calculadora/scripts.js
@@ -1,12 +1,13 @@
 let num1;
 let num2;
-let memory;
+let memory = 0;
 let symbol;
 let firstOperation = true;
+let display;
 
 function setDisplay(param) {
-  let display = document.getElementById("display");
-  if (display.value == 0 && firstOperation) {
+  display = document.getElementById("display");
+  if (firstOperation && param != "0") {
     display.value = param;
     firstOperation = false;
   } else {
@@ -18,5 +19,25 @@ function deleteOperation() {
   num1 = undefined;
   num2 = undefined;
   symbol = undefined;
-  display.value = "";
+  display.value = "0";
+  firstOperation = true;
+}
+
+function displayMemory() {
+  display = document.getElementById("display");
+  if (memory != 0) {
+    display.value = memory;
+  }
+}
+
+function addMemory() {
+  display = document.getElementById("display");
+  if (display.value != "0") {
+    memory += Number(display.value);
+  }
+  display.value = 0;
+}
+
+function clearMemory() {
+  memory = 0;
 }
