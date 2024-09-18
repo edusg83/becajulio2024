1235a6be4e26fac8bc1c98272e8d23d12b05899c
Nicolas <nicogilruz98@gmail.com>
2024-09-18 16:44:51 +0200

    Commit

---
+++ Ejercicios_JS/js/ejercicio17.js
@@ -0,0 +1,9 @@
+// a) Solicitar por pantalla un valor y guardarlo en una variable llamada nombre , mostrar el valor introducido por console.log. El
+// mensaje que tiene que aparecer al pedir el nombre es ‘Introduzca el nombre :’
+let nombre = prompt("Introduzca el nombre :");
+console.log(nombre);
+
+// a) Solicitar por pantalla una confirmación y guardarlo en una variable llamada continuar , mostrar el valor por console.log. El
+// mensaje que tiene que aparecer al pedir la confirmación es, ‘¿Desea continuar?’
+let continuar = confirm("¿Desea continuar?")
+console.log(continuar);
