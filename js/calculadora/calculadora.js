c0e71e71b4d19fd80c9f63edfd073480e6f8c585
Juarety <joaquingomezmar15@icloud.com>
2024-09-18 17:08:42 +0200

    la calculadora extra

--- js/calculadora/calculadora.js
+++ js/calculadora/calculadora.js
@@ -1,5 +1,6 @@
 let operacion = { num1: "", operador: "", num2: "" };
 let resultado = ""
+let sumatoriaM = 0
 
 function imprimirNumero(digito) {
 
@@ -14,10 +15,7 @@ function imprimirNumero(digito) {
 
         operacion.num2 += digito;
         document.getElementById("resultado").innerHTML = operacion.num1 + " " + operacion.operador + " " + operacion.num2;
-    } else if (resultado.length > 0) {
-
-    }
-    else {
+    } else {
         operacion.num1 += digito;
         document.getElementById("resultado").innerHTML = operacion.num1;
     }
@@ -36,6 +34,67 @@ function setoperator(ope) {
     }
 }
 
+function operacionesExtra(opcion) {
+    switch (opcion) {
+        case "mc":
+
+            sumatoriaM = 0;
+
+            break;
+        case "m_menos":
+            if (resultado != 0) {
+                sumatoriaM -= parseFloat(resultado);
+
+
+            } else if (operacion.operador.length != 0) {
+                sumatoriaM -= parseFloat(operacion.num2);
+
+            } else {
+                sumatoriaM -= parseFloat(operacion.num1);
+
+            }
+
+
+            break;
+        case "m_mas":
+            if (resultado != 0) {
+                sumatoriaM += parseFloat(resultado);
+
+
+            } else if (operacion.operador.length != 0) {
+                sumatoriaM += parseFloat(operacion.num2);
+
+            } else {
+                sumatoriaM += parseFloat(operacion.num1);
+
+            }
+
+            break;
+        case "mr":
+            if (resultado != 0) {
+                document.getElementById("resultado").innerHTML = sumatoriaM;
+                operacion.num1 = parseFloat(sumatoriaM);
+                resultado = "";
+
+            } else if (operacion.operador.length != 0) {
+                document.getElementById("resultado").innerHTML = operacion.num1 + " " + operacion.operador + " " + sumatoriaM;
+                operacion.num2 = parseFloat(sumatoriaM);
+            } else {
+                document.getElementById("resultado").innerHTML = sumatoriaM
+                operacion.num1 = parseFloat(sumatoriaM);
+            }
+            break;
+        case "c":
+            document.getElementById("resultado").innerHTML = ""
+            operacion = { num1: "", operador: "", num2: "" };
+
+
+            break;
+        default:
+            break;
+    }
+}
+
 function operacionTriunfo() {
 
     let primero = operacion.num1;
@@ -48,9 +107,7 @@ function operacionTriunfo() {
             } else {
                 resultado = parseFloat(primero) / parseFloat(segundo);
                 document.getElementById("resultado").innerHTML = resultado;
-
             }
-
             break;
         case "+":
             resultado = parseFloat(primero) + parseFloat(segundo);
