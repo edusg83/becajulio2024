00aa8d16a7ca0cef49eb9e3380befa1b1856cacb
rubenbanegas <sargolandia@gmail.com>
2024-09-19 15:50:59 +0200

    ejercicio17

---
+++ js/ejercicio16.js
@@ -0,0 +1,7 @@
+console.log("cokieEnabled: " + navigator.cookieEnabled);
+console.log("geolocation: " + navigator.geolocation)
+console.log("userAgent" + navigator.userAgent)
+console.log("language: " + navigator.language);
+console.log("permissions: " + navigator.permissions);
+console.log("online: " + navigator.onLine);
+console.log("credentials: " + navigator.credentials);
---
+++ js/ejercicio17.js
@@ -0,0 +1,8 @@
+//http://localhost:63342/Pruebas/bom/locaction.html?alfa=beta&ganma=delta
+let alfa = location.search.slice(6,10);
+let ganma = location.search.slice(17, location.search.length);
+
+// let params = new URLSearchParams(location.search);
+
+// let alfa2 = params.get("alfa");
+// let ganma2 = params.get("delta");
