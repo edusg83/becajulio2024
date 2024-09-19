540bd3b107f0d7fc841f63e7cae145fda746a0db
Cindy Zacarias <cindyjzacarias@gmail.com>
2024-09-19 16:19:17 +0200

    Ejercicio 18

---
+++ JS_EJE/Ejercicio_18/ejercicio18.js
@@ -0,0 +1,7 @@
+
+let tagName = document.getElementsByTagName("header");
+console.log(tagName);
+let byId = document.getElementById("cabecera");
+console.log(byId);
+let selector = document.querySelector("header");
+console.log(selector);
