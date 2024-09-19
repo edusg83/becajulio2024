b77537b6ebd055438d9cb8852051c4158e2cc093
EstherRuizSempere <estruisem@alu.edu.gva.es>
2024-09-19 19:31:22 +0200

    ejercicios

---
+++ JavaScript/tabla.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#miTabla').DataTable({
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.13.1/i18n/Spanish.json"
+        }
+    });
+});
