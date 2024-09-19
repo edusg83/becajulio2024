974070807b8f528e50086fa0bdcc2867cc0a9e91
Nicolas <nicogilruz98@gmail.com>
2024-09-19 17:04:49 +0200

    Commit

--- Parejas/js/parejas.js
+++ Parejas/js/parejas.js
@@ -1,14 +1,27 @@
 let eleccion1 = null;
 let eleccion2 = null;
-const imagenes = ["img/Squirtel.png", "img/Mew.png", "img/Bulbasaur.png", "img/Eevee.png", "img/Pikachu.png", "img/Vulpix.png", "img/Caterpie.png", "img/Seel.png", "img/Squirtel.png", "img/Mew.png", "img/Bulbasaur.png", "img/Eevee.png", "img/Pikachu.png", "img/Vulpix.png", "img/Caterpie.png", "img/Seel.png"]
-const imagenRandom = shuffe(imagenes);
-asignarImagenes(imagenes);
+const imagenes = ["img/Squirtel.png", "img/Mew.png", "img/Bulbasaur.png", "img/Eevee.png", "img/Pikachu.png", "img/Vulpix.png", "img/Caterpie.png", "img/Seel.png", "img/Squirtel.png", "img/Mew.png", "img/Bulbasaur.png", "img/Eevee.png", "img/Pikachu.png", "img/Vulpix.png", "img/Caterpie.png", "img/Seel.png"];
+
+barajarArray(imagenes);
+
+function reset() {
+    resetElecciones();
+    barajarArray(imagenes);
+}
+
+function barajarArray(imagenes) {
+    for (let i = imagenes.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [imagenes[i], imagenes[j]] = [imagenes[j], imagenes[i]];
+    }
+    asignarImagenes(imagenes);
+}
 
 function asignarImagenes(imagenesRandom) {
     let cartas = document.querySelectorAll(".tarjeta");
     cartas.forEach((tarjeta, index) => {
         if (index < imagenesRandom.length) {
-            let imagenCorrecta = tarjeta.querySelector(".imagen.correcta");
+            let imagenCorrecta = tarjeta.querySelector(".imagen-correcta");
 
             if (imagenCorrecta) {
                 imagenCorrecta.src = imagenesRandom[index];
@@ -18,8 +31,8 @@ function asignarImagenes(imagenesRandom) {
 }
 
 function guardarEleccion(carta) {
-    carta.querySelector('.imagen.oculta').style.display = 'none';
-    carta.querySelector('.imagen.correcta').style.display = 'block';
+    carta.querySelector('.imagen-oculta').style.display = 'none';
+    carta.querySelector('.imagen-correcta').style.display = 'block';
     carta.style.borderColor = 'orange';
 
     if (!eleccion1) {
@@ -31,8 +44,8 @@ function guardarEleccion(carta) {
 }
 
 function comprobarEleccion(carta1, carta2) {
-    let imagenCorrecta1 = carta1.querySelector('.imagen.correcta').src;
-    let imagenCorrecta2 = carta2.querySelector('.imagen.correcta').src;
+    let imagenCorrecta1 = carta1.querySelector('.imagen-correcta').src;
+    let imagenCorrecta2 = carta2.querySelector('.imagen-correcta').src;
 
     if (imagenCorrecta1 === imagenCorrecta2) {
         carta1.style.borderColor = 'green';
@@ -56,10 +69,10 @@ function resetElecciones() {
 }
 
 function resetEstilos(carta1, carta2) {
-    carta1.querySelector('.imagen.correcta').style.display = 'none';
-    carta2.querySelector('.imagen.correcta').style.display = 'none';
-    carta1.querySelector('.imagen.oculta').style.display = 'block';
-    carta2.querySelector('.imagen.oculta').style.display = 'block';
+    carta1.querySelector('.imagen-correcta').style.display = 'none';
+    carta2.querySelector('.imagen-correcta').style.display = 'none';
+    carta1.querySelector('.imagen-oculta').style.display = 'block';
+    carta2.querySelector('.imagen-oculta').style.display = 'block';
     carta1.style.borderColor = 'black';
     carta2.style.borderColor = 'black';
     resetElecciones();
