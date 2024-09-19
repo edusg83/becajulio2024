e88057702ec675800504012ad6a7d24215c7d557
EstherRuizSempere <estruisem@alu.edu.gva.es>
2024-09-19 16:20:12 +0200

    18

---
+++ JavaScript/ejercicio17.js
@@ -0,0 +1,7 @@
+header1 = document.getElementsByTagName('header');
+header2 = document.getElementById('headerId');
+header3 = document.querySelector('header');
+
+console.log("Get Element by Tag Name: " + header1);
+console.log("Get element by id: "  + header2);
+console.log("Query Selector: " + header3);
