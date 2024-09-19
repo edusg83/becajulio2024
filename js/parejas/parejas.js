47d77947a79bde71ca585c5fbb43f64ea53f0a78
Juarety <joaquingomezmar15@icloud.com>
2024-09-19 19:27:14 +0200

    detalles cartas

--- js/parejas/parejas.js
+++ js/parejas/parejas.js
@@ -5,7 +5,7 @@ let doblesparejas = ["1.png", "2.png", "3.png", "4.png", "5.png",
     "6.png", "7.png", "8.png", "9.png", "10.png",
     "11.png", "12.png", "13.png", "14.png", "15.png"]
 
-let parejasMezcladas = barajarArray(doblesparejas);
+let parejasMezcladas = barajarArray(doblesparejas)
 
 let jugada = { carta1: { id: '', src: '' }, carta2: { id: '', src: '' } }
 let puntuacio1 = 0
@@ -16,8 +16,15 @@ window.onload = function () {
 
     ocultarCartas()
     pedirJugadores();
-    //cambiarJugador();
 
+}
+
+function nuevaPartida() {
+    puntuacio1 = 0
+    puntuacio2 = 0
+    ocultarCartas()
+    parejasMezcladas = barajarArray(doblesparejas)
+    pedirJugadores()
 }
 
 function cambiarJugador() {
@@ -59,7 +66,7 @@ function revelar(id) {
             document.getElementById("score1").innerHTML = "Puntuacion: " + puntuacio1;
             document.getElementById("score2").innerHTML = "Puntuacion: " + puntuacio2;
             jugada = { carta1: { id: '', src: '' }, carta2: { id: '', src: '' } }
-            comprobarGanador((puntuacio1+puntuacio2))
+            comprobarGanador((puntuacio1 + puntuacio2))
         } else if (jugada.carta1.src.length > 0 && jugada.carta2.src.length > 0) {
             setTimeout(function () {
                 let img1 = document.getElementById("" + jugada.carta1.id); // Seleccionamos la imagen por ID
@@ -68,7 +75,7 @@ function revelar(id) {
                 img2.src = "img/reverso.png"
                 jugada = { carta1: { id: '', src: '' }, carta2: { id: '', src: '' } }
                 cambiarJugador()
-            }, 1000);
+            }, 750);
         }
     }
 }
@@ -89,17 +96,18 @@ function barajarArray(array) {
     }
     return array;
 }
-function comprobarGanador(i){
+function comprobarGanador(i) {
     console.log(i);
-    if(i == 15){
+    if (i == 15) {
         console.log("estoy")
-        if(puntuacio1 > puntuacio2){
-            prompt("Has resultado ganador, " + nombreJugador1+ " Felicidades, eres un maquina")
-        }else if(puntuacio2 > puntuacio1){
-            prompt("Has resultado ganador, " + nombreJugador2+ " Felicidades, eres un crack")
-        }else{
-            prompt("Increible, habeis empatado, Felicidades " +nombreJugador2 + " y " + nombreJugador1)
+        if (puntuacio1 > puntuacio2) {
+            alert("Has resultado ganador, " + nombreJugador1 + " Felicidades, eres un maquina")
+        } else if (puntuacio2 > puntuacio1) {
+            alert("Has resultado ganador, " + nombreJugador2 + " Felicidades, eres un crack")
+        } else {
+            alert("Increible, habeis empatado, Felicidades " + nombreJugador2 + " y " + nombreJugador1)
         }
+        nuevaPartida()
     }
 }
 
