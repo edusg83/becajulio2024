eecd3d788cd4540dc395a4460005ce5d465feb89
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-19 18:08:57 +0200

    F

--- Parejas/scripts.js
+++ Parejas/scripts.js
@@ -20,7 +20,22 @@ const valueMap = new Map();
 let player = {
   name: "name",
   hits: 0,
+  id: "",
+  playerTurn: false,
+  plusHit: function () {
+    this.hits++;
+  },
 };
+
+let player1;
+
+let player2;
+
+let num1;
+
+let num2;
+
+let isCardActive = false;
 
 for (let i = 0; i < imagenes.length; i++) {
   imageMap.set(imagenes[i], 0);
@@ -30,18 +45,33 @@ for (let i = 0; i < imagenes.length; i++) {
   valueMap.set(i, 0);
 }
 
-function setPlayerName() {
+function setPlayer() {
   let name = prompt("Hola! Cual es tu nombre?!");
 
-  let player1 = Object.create(player);
+  player1 = Object.create(player);
   player1.name = name;
+  player1.id = document.getElementById("player1_board");
+  player1.playerTurn = true;
 
-  let player2 = Object.create(player);
+  player2 = Object.create(player);
   player2.name = "Evil Eduardo";
+  player2.id = document.getElementById("player2_board");
+  player2.playerTurn = false;
 
-  document.getElementById("player1_name").value = player1.name;
-  console.log(document.getElementById("player1_name"));
-  document.getElementById("player2_name").value = player2.name;
+  document.getElementById("player1_board").value = player1.name;
+  document.getElementById("player2_board").value = player2.name;
+}
+
+function setPlayerTurn() {
+  let display = document.getElementById("turn_display");
+
+  display.value = "";
+
+  if (player1.playerTurn) {
+    display.value = "Turno de " + player1.name;
+  } else {
+    display.value = "Turno de " + player2.name;
+  }
 }
 
 function randomGenerator() {
@@ -84,6 +114,53 @@ function toggleEffect(param) {
   let button = document.getElementById(param);
   button.classList.toggle("rotateEffect");
   button.children[0].classList.toggle("opacity-100");
+  button.classList.add("activo");
 }
 
-function checkCard() {}
+function toggleBack() {
+  const buttons = document.getElementsByClassName("activo");
+
+  console.log(buttons);
+
+  for (let element of buttons) {
+    element.classList.toggle("rotateEffect");
+    element.children[0].classList.toggle("opacity-100");
+    element.classList.remove("activo");
+  }
+}
+
+function storeCardValue(param) {
+  if (isCardActive) {
+    num2 = param;
+    checkValue();
+  } else {
+    num1 = param;
+    isCardActive = true;
+  }
+}
+
+function checkValue() {
+  if (num1 == num2) {
+    if (player1.playerTurn) {
+      player1.plusHit();
+    } else {
+      player2.plusHit();
+    }
+  } else {
+    toggleBack();
+    isCardActive = false;
+    if (player1.playerTurn) {
+      player1.playerTurn = false;
+      player2.playerTurn = true;
+    } else {
+      player1.playerTurn = true;
+      player2.playerTurn = false;
+    }
+  }
+
+  num1 = undefined;
+  num2 = undefined;
+
+  document.getElementById("score_display_player1").value = player1.hits;
+  document.getElementById("score_display_player2").value = player2.hits;
+}
