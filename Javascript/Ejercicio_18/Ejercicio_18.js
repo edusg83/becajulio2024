ef3a143eedeab400c1152ecc787981e83a786dba
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-19 16:19:57 +0200

    nyohm

---
+++ Javascript/Ejercicio_18/Ejercicio_18.js
@@ -0,0 +1,5 @@
+let x = document.getElementById("head");
+
+console.log(x);
+console.log(document.getElementsByTagName("header"));
+console.log(document.querySelector("header"));
--- Parejas/scripts.js
+++ Parejas/scripts.js
@@ -1,4 +1,4 @@
-let cards = Array.from(document.getElementsByClassName("card"));
+const cards = Array.from(document.getElementsByClassName("card"));
 
 const imagenes = [
   "/Parejas/resources/Squirtle.jpg",
@@ -11,48 +11,53 @@ const imagenes = [
   "/Parejas/resources/Charmander.jpg",
 ];
 
-const test = new Map();
+//Mapa para controlar la repeticion de imagenes
+const imageMap = new Map();
+
+//Mapa para controlar los valores asignados a cada carta
+const valueMap = new Map();
 
 for (let i = 0; i < imagenes.length; i++) {
-  test.set(imagenes[i], Number(0));
+  imageMap.set(imagenes[i], 0);
 }
 
-console.log(test); //DEVUELVE MAPA ENTERO
-console.log(test.get(imagenes[1])); //DEVUELVE VALOR
+for (let i = 0; i < imagenes.length; i++) {
+  valueMap.set(i, 0);
+}
+
+//ACUERDATEEEEE
+console.log(imageMap); //DEVUELVE MAPA ENTERO
+console.log(imageMap.get(imagenes[1])); //DEVUELVE VALOR
 
 function randomGenerator() {
-  let ranNum;
-
-  ranNum = Math.floor(Math.random() * imagenes.length);
-
-  return ranNum;
+  return Math.floor(Math.random() * imagenes.length);
 }
 
-// function asignValue() {
-//   cards.forEach((element) => {
-//     element.value = randomGenerator();
-//   });
-// }
+function asignValue() {
+  cards.forEach((element) => {
+    element.value = randomGenerator();
+  });
+}
 
 function assignImage() {
   cards.forEach(function (card) {
     let index = randomGenerator();
 
-    while (test.get(imagenes[index]) == 2) {
+    while (imageMap.get(imagenes[index]) == 2) {
       index = randomGenerator();
     }
 
     card.children[0].style.backgroundImage = `url(${imagenes[index]})`;
 
     //POR QUE NO DEJA OPERAR DIRECTAMENTE SOBRE EL VALOR?!?!?!?!?!
-    let suma = test.get(imagenes[index]) + 1;
+    let suma = imageMap.get(imagenes[index]) + 1;
 
-    test.set(imagenes[index], suma);
+    imageMap.set(imagenes[index], suma);
   });
 }
 
 function toggleEffect(param) {
   let button = document.getElementById(param);
-  button.classList.toggle("test");
+  button.classList.toggle("rotateEffect");
   button.children[0].classList.toggle("opacity-100");
 }
