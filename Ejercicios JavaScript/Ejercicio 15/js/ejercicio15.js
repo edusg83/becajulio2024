5c855936289fdab5170a404e0e4cf62c7ab2c590
Jesús Terrés <jesusterres1999@gmail.com>
2024-09-22 20:08:44 +0200

    Ejercicio 15 JS

---
+++ Ejercicios JavaScript/Ejercicio 15/js/ejercicio15.js
@@ -0,0 +1,11 @@
+// Solicitar un nombre y guardarlo en la variable 'nombre'
+let nombre = prompt('Introduzca el nombre:');
+
+// Mostrar el valor introducido por el usuario en la consola
+console.log('Nombre introducido:', nombre);
+
+// Solicitar una confirmación y guardarla en la variable 'continuar'
+let continuar = confirm('¿Desea continuar?');
+
+// Mostrar el valor de la confirmación en la consola
+console.log('¿Desea continuar?', continuar);
