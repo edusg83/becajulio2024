762bcd90cb483d946b1088d26848bd49c57a5441
Cindy Zacarias <cindyjzacarias@gmail.com>
2024-09-23 15:20:27 +0200

    Din{amico

---
+++ JS_EJE/Ejercicio_19/ejercicio19.js
@@ -0,0 +1,11 @@
+
+function elementoDinamico() {
+
+    const etiquetaP = document.createElement("p");
+    const textoNodo = document.createTextNode("Este texto está añadido dinámicamente");
+    etiquetaP.appendChild(textoNodo);
+    document.querySelector("body").appendChild(etiquetaP);
+
+}
+
+elementoDinamico();
