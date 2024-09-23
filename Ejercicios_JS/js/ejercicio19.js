6e0ab8a291ff466c9796b8de0027591b40a5fecb
Nicolas <nicogilruz98@gmail.com>
2024-09-23 15:20:11 +0200

    Commit

--- Ejercicios_JS/js/ejercicio19.js
+++ Ejercicios_JS/js/ejercicio19.js
@@ -1,9 +1,8 @@
-// a) Solicitar por pantalla un valor y guardarlo en una variable llamada nombre , mostrar el valor introducido por console.log. El
-// mensaje que tiene que aparecer al pedir el nombre es ‘Introduzca el nombre :’
-let nombre = prompt("Introduzca el nombre :");
-console.log(nombre);
+(function () {
+    let doc = document,
+        elem = doc.createElement("p"),
+        contenido = doc.createTextNode("Este texto está añadido dinámicamente");
 
-// a) Solicitar por pantalla una confirmación y guardarlo en una variable llamada continuar , mostrar el valor por console.log. El
-// mensaje que tiene que aparecer al pedir la confirmación es, ‘¿Desea continuar?’
-let continuar = confirm("¿Desea continuar?")
-console.log(continuar);
+    elem.appendChild(contenido);
+    document.body.appendChild(elem);
+})();
