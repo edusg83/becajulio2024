a93fdae0fac3a93d00d0d33fe8159dfe5318854a
Nicolas <nicogilruz98@gmail.com>
2024-09-24 16:32:03 +0200

    Si

--- Ejercicios_JS/js/ejercicio23.js
+++ Ejercicios_JS/js/ejercicio23.js
@@ -1,10 +1,41 @@
 const request = new Request("https://eoi.wiremockapi.cloud/clientes");
 
+let tabla = `
+<table id="dataTable">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nombre</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+`;
+
+let finTabla = `
+</tbody>
+</table>
+`;
+
+let filas = ``;
+
 fetch(request)
     .then(response => response.json())
     .then(data => {
         data.UserListData.forEach(user => {
-            console.log(user)
+            filas+=`
+                <tr>
+                    <td>${user.id}</td>
+                    <td>${user.username}</td>
+                    <td>${user.nombre}</td>
+                    <td>${user.email}</td>
+                </tr>
+            `;
         })
+
+        tabla += filas + finTabla;
+        document.getElementById("resultados").innerHTML = tabla;
     })
     .catch(error => console.error('Error:', error));
+
