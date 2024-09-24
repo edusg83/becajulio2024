a0dfba06ee605ceb4c4719d2464675f5d4f62857
Cindy Zacarias <cindyjzacarias@gmail.com>
2024-09-24 16:28:31 +0200

    Ejercicio 2 tabla

--- API_EJE/Ejercicio_2/script.js
+++ API_EJE/Ejercicio_2/script.js
@@ -5,14 +5,15 @@ const URL = request.url;
 const method = request.method;
 const credentials = request.credentials;
 
-console.log(URL);
-console.log(method);
-console.log(credentials);
+let table = document.getElementById("lista");
 
 fetch(request)
 .then(response => response.json())
 .then(data => {
     data.UserListData.forEach(i => {
         console.log(i);
+        table.innerHTML += "<tr><td>"+ i.id + i.username + i.nombre + i.email +"</td></tr>";
     })
 })
+
+
