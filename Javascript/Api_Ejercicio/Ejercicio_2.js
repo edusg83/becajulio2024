44096b2245a5b05cb5080040bf8078e7dd7731f1
manu <144843962+ManusolJ@users.noreply.github.com>
2024-09-24 16:55:23 +0200

    im tired boss

--- Javascript/Api_Ejercicio/Ejercicio_2.js
+++ Javascript/Api_Ejercicio/Ejercicio_2.js
@@ -1,11 +1,38 @@
-const request = "https://eoi.wiremockapi.cloud/clientes";
-
-const URL = request.url;
-const method = request.method;
-const credentials = request.credentials;
+const request = new Request("https://eoi.wiremockapi.cloud/clientes");
 
 fetch(request)
     .then(response => response.json())
     .then(data => {
-        User
+
+        console.log(data);
+
+        let tabla = `<table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Username</th>
+                <th>Nombre</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>`
+
+        let finTabla = `</tbody>
+        </table>`
+
+        let filas = '';
+
+        data.UserListData.array.forEach(user => {
+            filas += `
+            <tr>
+                <td>${user.id}</td>
+                <td>${user.username}</td>
+                <td>${user.nombre}</td>
+                <td>${user.email}</td>
+            </tr>`
+
+        });
+
+        tabla += filas + finTabla;
+        document.getElementById("resultado").innerHTML = tabla;
     })
